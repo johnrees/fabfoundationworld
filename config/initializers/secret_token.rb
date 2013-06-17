b0f1572d16f099b752d52379b70f7aff58e4f8c5
johnrees <john@bitsushi.com>
2013-06-17 20:25:02 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fabfoundationworld::Application.config.secret_key_base = '7815a491883fda9157e9de2745b1acb375a58d16abed4a8f0835a5e736b8a17ccacba931224664389d57029793e02d7188eb32dc733150202eed18f093b53206'
