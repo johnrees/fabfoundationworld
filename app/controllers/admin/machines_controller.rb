7061a6a1239604cd1e74f03601f910af40bfa8fb
johnrees <john@bitsushi.com>
2013-06-25 23:39:59 +0200

    can create machine

--- app/controllers/admin/machines_controller.rb
+++ app/controllers/admin/machines_controller.rb
@@ -1,2 +1,31 @@
 class Admin::MachinesController < Admin::AdminController
+  before_action :set_machine, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @machine = Machine.new
+  end
+
+  def index
+    @machines = Machine.all
+  end
+
+  def create
+    @machine = Machine.new(machine_params)
+    if @machine.save
+      redirect_to admin_machines_path, notice: 'Machine was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+private
+
+  def set_machine
+    @machine = Machine.find(params[:id])
+  end
+
+  def machine_params
+    params.require(:machine).permit(:name)
+  end
+
 end
--- spec/features/machines_spec.rb
+++ spec/features/machines_spec.rb
@@ -3,6 +3,23 @@ require 'spec_helper'
 describe "Machines" do
   it "has index" do
     visit admin_machines_path
-    page.should have_selector 'h1', text: "Machines"
+    expect(page).to have_selector 'h1', text: "Machines"
   end
+
+  it "can add machine" do
+    visit admin_root_path
+    click_link "Machines"
+    click_link "Add Machine"
+    fill_in "Name", with: "Machine Name"
+    click_button "Create"
+    expect(page).to have_selector '#notice', text: "Machine was successfully created."
+    expect(page).to have_link "Machine Name"
+  end
+
+  it "lists machines" do
+    FactoryGirl.create(:machine, name: 'Machine A')
+    visit admin_machines_path
+    expect(page).to have_link "Machine A"
+  end
+
 end
